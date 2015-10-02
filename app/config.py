498d144a6c3560a6e0c7e8aae8f948782acf6d64
Robert <robert.david.west@gmail.com>
2015-10-01 22:37:31 -0400

    added details of pythonanywhere-mysql db in config file. just comments right now

--- app/config.py
+++ app/config.py
@@ -13,3 +13,14 @@ DATABASE_PATH = os.path.join(basedir, DATABASE)
 
 # the database uri
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
+
+# settings for pythonanywhere mysql database #
+
+#DATABASE = 'robertdavidwest$fx_quotes'
+#HOSTNAME = 'robertdavidwest.mysql.pythonanywhere-services.com/'
+#USERNAME = 'robertdavidwest'
+#MYSQL_PASSWORD = 'testtest'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{}:{}@{}/{}'.format(USERNAME,
+#                                                               MYSQL_PASSWORD,
+#                                                               HOSTNAME,
+#                                                               DATABASE)
--- app/db_create.py
+++ app/db_create.py
@@ -2,7 +2,6 @@
 
 from datetime import datetime
 import pytz
-from pytz import timezone
 from views import db
 from models import FXQuotes
 
